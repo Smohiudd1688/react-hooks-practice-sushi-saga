17a4793a1221aec35b054d4e5a82f48b3f7c9809
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-06-21 19:50:32 -0700

    commit tree c4d08adcd35663123e53d3503c1fc81cd02ebea5

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushiList, setSushiList] = useState([]);
+  const [emptyPlates, setEmptyPlates] = useState([]);
+  const [money, setMoney] = useState(400);
+
+  function handleEmptyPlates(sushi) {
+    setEmptyPlates([...emptyPlates, sushi]);
+  }
+
+  function handleMoneyDecrease(price) {
+    setMoney(money => money - price);
+  }
+
+  useEffect(() => {
+    fetch(API)
+    .then(response => response.json())
+    .then(sushi => setSushiList(sushi))
+  }, []);
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushiList={sushiList} money={money} onEmptyPlates={handleEmptyPlates} onMoneyDecrease={handleMoneyDecrease} />
+      <Table money={money} plates={emptyPlates} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
+import Sushi from "./Sushi";
 import MoreButton from "./MoreButton";
 
-function SushiContainer(props) {
+function SushiContainer({sushiList, money, onEmptyPlates, onMoneyDecrease}) {
+  const [sushiIndex, setSushiIndex] = useState(0);
+  const sushiDisplay = [];
+
+  function handleMoreClick() {
+    setSushiIndex(sushiIndex => sushiIndex + 4);
+  }
+
+  if (sushiList.length !== 0) {
+    for (let i = sushiIndex; i < sushiIndex + 4; i++) {
+      sushiDisplay.push(sushiList[i]);
+    }
+  }
+
+  const renderSushi = sushiDisplay?.map(sushi => {
+    return <Sushi 
+                key={sushi.id}
+                image={sushi.img_url}
+                name={sushi.name} 
+                price={sushi.price}
+                money={money}
+                onEmptyPlates={onEmptyPlates}
+                onMoneyDecrease={onMoneyDecrease}
+          />
+  });
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {renderSushi}
+      <MoreButton onMoreClick={handleMoreClick} />
     </div>
   );
 }
